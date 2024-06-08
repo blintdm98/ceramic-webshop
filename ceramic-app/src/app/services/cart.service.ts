da0eb632ea20c375613e1c277ea12fd7d3b8e54a
blintdm98@gmail.com <blintdm98@gmail.com>
2024-06-08 11:39:18 +0300

    styled cart overlay

--- ceramic-app/src/app/services/cart.service.ts
+++ ceramic-app/src/app/services/cart.service.ts
@@ -8,12 +8,10 @@ export class CartService {
   cartOpen = new EventEmitter<boolean>();
 
   openCart() {
-    console.log(this.cartOpen)
     this.cartOpen.emit(true);
   }
 
   closeCart() {
-    console.log(this.cartOpen.emit(false));
     this.cartOpen.emit(false);
   }
 
